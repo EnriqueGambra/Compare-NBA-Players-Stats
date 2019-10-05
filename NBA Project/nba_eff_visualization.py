25bae73618fe1361fe42ca630f839d8851cfd25f
Enrique Gambra <gambra7@gmail.com>
2019-10-05 10:27:03 -0400

    Added in some more comments to make code more readable.

--- NBA Project/nba_eff_visualization.py
+++ NBA Project/nba_eff_visualization.py
@@ -1,6 +1,5 @@
 from get_id import GetID
 from get_stats import GetStats
-import plotly.graph_objs as go
 from plotly import offline
 
 num_of_players = int(input("Welcome! Please enter the number of players you'd like to analyze." +
@@ -34,9 +33,11 @@ for number in range(num_of_players):
             get_stats_instance = GetStats(player_id)
             players_stats.append(get_stats_instance.stats_dict)
 
+# Enters in players stats into a list
 for player in players_stats:
     all_stats.append(f"PTS: {player['PTS']} REB: {player['REB']} AST: {player['AST']}, FG PCT: {player['FG_PCT']}")
 
+# Customizing the plot
 data = [{
     'type': 'bar',
     'x': players_names,
@@ -49,6 +50,7 @@ data = [{
         },
 }]
 
+# Customizing the layout and labels
 my_layout = {
     'title': "Average PPG for a Player's Career",
     'titlefont': {'size': 28},
