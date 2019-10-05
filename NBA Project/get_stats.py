16ce8bb7a89622ffd91fee6926b61e4d26628e4c
Enrique Gambra <gambra7@gmail.com>
2019-10-04 23:05:52 -0400

    Refactored get_stats.py into a class. When called upon, will return stats of the desired player. Next commit will refactor the class further to implement encapsulation so the file doesn't directly manipulate variables.

--- NBA Project/get_stats.py
+++ NBA Project/get_stats.py
@@ -1,38 +1,58 @@
 from nba_api.stats.endpoints import playercareerstats
 import json
 
-filename = 'data/player_stats.json'
 
-data = playercareerstats.PlayerCareerStats(player_id=2544)
-data_json = data.get_dict()
+class GetStats:
+    """Class that will compute and return the statistics for a given player ID"""
 
-with open(filename, 'w') as f:
-    json.dump(data_json, f, indent=4)
+    def __init__(self, player_id):
+        """Initializes the GetStats class"""
+        self.player_id = player_id
+        self.filename = 'data/player_stats.json'
+        self._create_file()
+        self._open_file()
 
-with open(filename) as f:
-    formatted_data = json.load(f)
+    def _create_file(self):
+        """Method that will create the correct corresponding JSON file according to the player id passed."""
+        data = playercareerstats.PlayerCareerStats(player_id=self.player_id)
+        data_json = data.get_dict()
 
-    data_header = formatted_data["resultSets"][0]['headers']
-    data_stats = formatted_data["resultSets"][0]["rowSet"]
-    games, points, rebounds, assists, field_goal_pct = [], [], [], [], []
+        with open(self.filename, 'w') as f:
+            json.dump(data_json, f, indent=4)
 
-    for row in data_stats:
-        games_played = row[6]
-        pts = row[26] / games_played
-        rbs = row[20] / games_played
-        ast = row[21] / games_played
-        fg_pct = row[11]
+    def _open_file(self):
+        """Method that will open the JSON file and call a helper method to extract the data."""
+        with open(self.filename) as f:
+            formatted_data = json.load(f)
 
-        points.append(pts)
-        rebounds.append(rbs)
-        assists.append(ast)
-        field_goal_pct.append(fg_pct)
+        data_stats = formatted_data["resultSets"][0]["rowSet"]
 
-    avg_points = sum(points) / len(points)
-    avg_rebounds = sum(rebounds) / len(rebounds)
-    avg_assists = sum(assists) / len(assists)
-    avg_fg_pct = sum(field_goal_pct) / len(field_goal_pct)
+        self._compute_stats(data_stats)
 
+    def _compute_stats(self, data):
+        """Method that will compute the averages of all necessary stats that we want."""
+        games, points, rebounds, assists, field_goal_pct = [], [], [], [], []
 
-print(avg_assists)
-# print(avg_points, avg_rebounds, avg_assists, avg_fg_pct)
+        for row in data:
+            games_played = row[6]
+            pts = row[26] / games_played
+            rbs = row[20] / games_played
+            ast = row[21] / games_played
+            fg_pct = row[11]
+
+            points.append(pts)
+            rebounds.append(rbs)
+            assists.append(ast)
+            field_goal_pct.append(fg_pct)
+
+        avg_points = sum(points) / len(points)
+        avg_rebounds = sum(rebounds) / len(rebounds)
+        avg_assists = sum(assists) / len(assists)
+        avg_fg_pct = sum(field_goal_pct) / len(field_goal_pct)
+
+        self.stats_dict = {
+            'PTS': avg_points,
+            'REB': avg_rebounds,
+            'AST': avg_assists,
+            'FG_PCT': avg_fg_pct
+        }
---
+++ NBA Project/testing.py
@@ -0,0 +1,4 @@
+from get_stats import GetStats
+
+instance = GetStats(2544)
+print(instance.stats_dict)
