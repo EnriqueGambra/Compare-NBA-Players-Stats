16922366e3c911331454ed28dab61fe0ae879227
Enrique Gambra <gambra7@gmail.com>
2019-10-04 22:44:01 -0400

    Successfully created a test script that gets Lebron James' career averages in the regular season for points, rebounds, assists, and field goal percentage. Will now work on creating a class that will allow any player_id to be passed and get the same exact information

--- NBA Project/get_stats.py
+++ NBA Project/get_stats.py
@@ -13,5 +13,26 @@ with open(filename) as f:
     formatted_data = json.load(f)
 
     data_header = formatted_data["resultSets"][0]['headers']
-    for index, header in enumerate(data_header):
-        print(index, header)
+    data_stats = formatted_data["resultSets"][0]["rowSet"]
+    games, points, rebounds, assists, field_goal_pct = [], [], [], [], []
+
+    for row in data_stats:
+        games_played = row[6]
+        pts = row[26] / games_played
+        rbs = row[20] / games_played
+        ast = row[21] / games_played
+        fg_pct = row[11]
+
+        points.append(pts)
+        rebounds.append(rbs)
+        assists.append(ast)
+        field_goal_pct.append(fg_pct)
+
+    avg_points = sum(points) / len(points)
+    avg_rebounds = sum(rebounds) / len(rebounds)
+    avg_assists = sum(assists) / len(assists)
+    avg_fg_pct = sum(field_goal_pct) / len(field_goal_pct)
+
+
+print(avg_assists)
+# print(avg_points, avg_rebounds, avg_assists, avg_fg_pct)
